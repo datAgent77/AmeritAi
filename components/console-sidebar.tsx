3aae78bb19fbe3785480e3a83979e9046dc64fc5
yasincelenk <yasincelenkk@gmail.com>
2026-01-16 22:32:45 +0300

    disable: Developer Program menu item

--- components/console-sidebar.tsx
+++ components/console-sidebar.tsx
@@ -495,7 +495,7 @@ export function ConsoleSidebar({ targetUserId, targetEmail, sectorId }: ConsoleS
                                                 </div>
                                             </div>
                                         </DropdownMenuItem>
-                                        <DropdownMenuItem onClick={() => router.push("/console/settings/developers")} className="px-2 py-2 cursor-pointer">
+                                        <DropdownMenuItem disabled className="px-2 py-2 cursor-pointer">
                                             <div className="flex items-center gap-3">
                                                 <div className="flex items-center justify-center size-8 rounded-full bg-gray-100 dark:bg-zinc-800">
                                                     <Code className="size-4" />
